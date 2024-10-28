818dda1bea7cbfe66aaed71686e1bba25f517162
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-10-28 21:11:47 +0000

    Update lemp_stack_automate.sh
    Added code to run wordpress_automate.sh script

--- lemp_stack_automate.sh
+++ lemp_stack_automate.sh
@@ -20,3 +20,6 @@ sudo systemctl status mariabd >> /root/testing.txt
 # Install PHP 
 sudo apt -y install php-fpm php php-cli php-common php-imap php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
 sudo php -v >> /root/testing.txt
+
+# Run the wordpress_automate.sh script 
+sudo bash /root/WordPressPractise/wordpress_automate.sh
