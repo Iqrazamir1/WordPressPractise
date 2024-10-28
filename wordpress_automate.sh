8293c74495250671e84532cc0512017bc9472eb3
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-10-28 21:28:59 +0000

    Create wordpress_automate.sh
    Added the wordpress install script

---
+++ wordpress_automate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install unzip Package 
+sudo apt -y install unzip
+
+# Install/Unzip/Remove WordPress 
+sudo wget https://wordpress.org/latest.zip
+sudo unzip latest.zip
+sudo rm latest.zip
+
+# Create MariaDB Database
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
+sudo mysql -e "CREATE USER IF NOT EXISTS wpuser@localhost identified by 'my_password'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
+sudo mysql -e "FLUSH PRIVILEGES"
+
+sudo wget -O /var/www/html/wp-config.php https://iqrawordpressbucket.s3.eu-north-1.amazonaws.com/wp-config.php
+sudo chmod 640 /var/www/html/wp-config.php
+sudo chown -R www.data:www-data /var/www/html 
