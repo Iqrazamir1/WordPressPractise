e46deffb1e9052fee74987c325f89850286d10ac
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-10-30 22:52:20 +0000

    Update lemp_stack_automate.sh
    Committed the rename apache testing page code

--- lemp_stack_automate.sh
+++ lemp_stack_automate.sh
@@ -18,7 +18,7 @@ sudo apt -y install php-fpm php php-cli php-common php-imap  php-snmp php-xml ph
 sudo php -v >> /root/testing.txt
 
 # Renaming apache testing page 
-sudo mv /var/www/html/index.html /var/www/html/index.html.old 
+# sudo mv /var/www/html/index.html /var/www/html/index.html.old 
 
 sudo mv /root/WordPressPractise/nginx.conf /etc/nginx/conf.d/nginx.conf
 
