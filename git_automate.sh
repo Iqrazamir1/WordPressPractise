a3efe98dd38b34481e55dc62ff5926a2de1f2559
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-10-30 18:14:37 +0000

    Update git_automate.sh
    Added update/upgrade command

--- git_automate.sh
+++ git_automate.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Install Git 
-sudo apt -y install git 
-cd /root/
-sudo git clone https://github.com/Iqrazamir1/WordPressPractise.git
-sudo chmod -R 755 WordPressPractise
-sudo bash WordPressPractise/lemp_stack_automate.sh
+# Updates all the latest security patches and software packages to ensure the highest level of security for my deployment.
+sudo apt -y update 
+sudo apt -y upgrade 
+
+sudo git clone https://github.com/Iqrazamir1/WordPressPractise.git /root/WordPressPractise
+
+sudo chmod -R 755 /root/WordPressPractise
+sudo bash /root/WordPressPractise/lemp_stack_automate.sh
