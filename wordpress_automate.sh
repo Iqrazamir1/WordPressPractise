5e3accf6a903f099c8a31637e5043925e8e269a9
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2024-12-11 15:45:48 +0000

    Update wordpress_automate.sh
    Added code to auto generate passwords

--- wordpress_automate.sh
+++ wordpress_automate.sh
@@ -11,15 +11,22 @@ sudo wget https://wordpress.org/latest.zip
 sudo unzip latest.zip
 sudo rm latest.zip
 
+# Generate Passwords for the WP Database 
+password=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 25)
+username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 25)
+
+echo $password > creds.txt
+echo $username >> creds.txt
+
 # Create a MariaDB Database and a User for the WordPress Site  
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
-sudo mysql -e "CREATE USER wpuser@localhost identified by 'my_password'"
-sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
+sudo mysql -e "CREATE USER wpuser@localhost identified by '$password'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* to $username@localhost"
 sudo mysql -e "FLUSH PRIVILEGES" # Applies everything you've done 
 
-# Configure WordPress
-#cd wordpress/
-#cp wp-con#ig-sample.php wp-config.php 
+sed -i "s/password_here/$password/g" /var/www/html/wp-config.php
+sed -i "s/username_here/$username/g" /var/www/html/wp-config.php
+sed -i "s/database_name_here/$username/g" /var/www/html/wp-config.php
 
 sudo wget -O /var/www/html/wordpress/wp-config.php https://iqrawordpressbucket.s3.eu-north-1.amazonaws.com/wp-config.php
 
