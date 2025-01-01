abbefd57f382af22a52dfc3e1a2ccba0716b7a86
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2025-01-01 17:10:45 +0000

    Update git_automate.sh

--- git_automate.sh
+++ git_automate.sh
@@ -28,17 +28,17 @@ check_exit_status "apt upgrade"
 
 # Clone the GitHub repository
 echo "Cloning GitHub repository..." | tee -a $LOG_FILE
-sudo git clone https://github.com/saxtonator/wordpress-project.git /root/wordpress-project
+sudo git clone -b test https://github.com/Iqrazamir1/WordPressPractise.git
 check_exit_status "git clone"
 
 # Change permissions of the cloned repository
 echo "Changing permissions of the cloned repository..." | tee -a $LOG_FILE
-sudo chmod -R 755 /root/wordpress-project
+sudo chmod -R 755 /root/WordPressPractise
 check_exit_status "chmod"
 
 # Run the setup script
 echo "Running lemp-setup.sh script..." | tee -a $LOG_FILE
-
+sudo bash /root/WordPressPractise/lemp_stack_automate.sh
 
 ############################################################################
 
