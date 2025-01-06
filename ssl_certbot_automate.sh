e75daab265237b61decce24d0fe3a4f3e49c2ddc
Iqrazamir1 <89987144+Iqrazamir1@users.noreply.github.com>
2025-01-06 09:59:00 +0000

    Update ssl_certbot_automate.sh

--- ssl_certbot_automate.sh
+++ ssl_certbot_automate.sh
@@ -4,7 +4,14 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y certbot
 sudo apt install -y python3-certbot-nginx
-sudo certbot --nginx
+
+# Define your email
+EMAIL="zamiriqra0@outlook.com"
+DOMAIN="certbot.paints-4-you.com"
+
+# Use Certbot to obtain and install the SSL certificate
+sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL -d $DOMAIN
+
 systemctl reload nginx
 
 sudo bash /root/WordPressPractise/wordpress_automate.sh 
